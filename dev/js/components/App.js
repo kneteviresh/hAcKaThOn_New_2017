1f64deae8b15b0e84b71be95a6f5e402e8ee36d0
Kavitha <xx18@global.tesco.org>
2017-09-21 17:27:55 +0530

    Added page to navigate from login to items list

--- dev/js/components/App.js
+++ dev/js/components/App.js
@@ -4,12 +4,40 @@ import Items from './homeScreen/Items';
 import Login from './login/login';
 
 class App extends Component {
+    constructor(props,context)
+    {
+        super(props, context);
+        this.state = {
+            isItemsPageVisible : false,
+            isLoginPageVisible : true
+            }
+        this.showItemList = this.showItemList.bind(this);
+        this.showLoginPage = this.showLoginPage.bind(this);
+        this.onScreenChange = this.onScreenChange.bind(this);
+    }
+        showItemList(){
+            if(this.state.isItemsPageVisible){
+                return <Items/>
+            }
+        }
+        showLoginPage(){
+            if(this.state.isLoginPageVisible){
+                return <Login screenChange = {this.onScreenChange}/>
+            }
+        }
+        onScreenChange(){
+            this.setState({
+                isItemsPageVisible : true,
+                isLoginPageVisible : false
+
+            })
+        }
     render() {
         return (
             <div>
-                <h1>hello Viresh</h1>
-                <Items />
-                <Login/>
+                {this.showLoginPage()}
+                {this.showItemList()}
+
             </div>
         );
     }
