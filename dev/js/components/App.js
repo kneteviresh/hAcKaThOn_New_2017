a429cc3b95c6a09f7c23a1689b0f675794dd6f0a
uf62 <uf62@global.tesco.org>
2017-09-21 19:57:06 +0530

    changes

--- dev/js/components/App.js
+++ dev/js/components/App.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import Header from './homeScreen/Header';
 import Items from './homeScreen/Items';
 import Login from './login/login';
+import SelectedProduct from './productView/SelectedProduct';
 
 class App extends Component {
     render() {
         return (
             <div>
-                <Header />
-                 <Items />
-                <Login/> 
+                {/* <Header />
+                <Items />
+                <Login/>  */}
+                <SelectedProduct />
             </div>
         );
     }
