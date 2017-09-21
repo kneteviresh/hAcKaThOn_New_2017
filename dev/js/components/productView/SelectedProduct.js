77683e4f73e9323f2818f5d8ef64266af6b138f0
de36 <veeresh.kashappa@in.tesco.com>
2017-09-21 23:00:34 +0530

    latest

--- dev/js/components/productView/SelectedProduct.js
+++ dev/js/components/productView/SelectedProduct.js
@@ -6,44 +6,47 @@ import women from '../../../images/women.jpg';
 class SelectedProduct extends Component {
     constructor(props, context) {
         super(props, context);
-            this.renderButtons = this.renderButtons.bind(this);
+
+        this.handleAddToCart = this.handleAddToCart.bind(this);
     }
-    
-    renderButtons(){
-    
+
+    handleAddToCart(){
+        this.props.handleViewChange('cart')
     }
     render() {
         return (
             <div className="boldFont">
-                <div className="container" style={{ marginTop: "200px"}}>
+                <div className="container" style={{ marginTop: "200px" }}>
                     <div className="row">
-                    <div className="col-md-2">
-                        <img className="img-responsive resizeImage" src={men} />
+                        <div className="col-md-2">
+                            <img className="img-responsive resizeImage" src={men} />
+                        </div>
+                        <div className="col-md-6">
+                            <table>
+                                <tr>
+                                    <td className="marginItem"> ItemDescription:</td>
+                                    <td> Proline: Men T-shirt</td>
+                                </tr>
+                                <tr className="marginItem">
+                                    <td> Price: </td>
+                                    <td>  </td>
+                                </tr >
+                                <tr className="marginItem">
+                                    <td> Quantity: </td>
+                                    <td>   </td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div className="col-md-4">
+
+                        </div>
                     </div>
-                    <div className="col-md-6">
-                        <table>
-                        <tr>
-                           <td className="marginItem"> ItemDescription:</td>
-                           <td> Proline: Men T-shirt</td>
-                        </tr>
-                        <tr className="marginItem">
-                           <td> Price: </td>
-                           <td>  </td>
-                        </tr >
-                         <tr className="marginItem">
-                           <td> Quantity: </td>
-                           <td>   </td>
-                        </tr>
-                        </table>
+                    <div style={{ marginTop: "50px" }}><button type="button" className="btn btn-primary">Share n Save</button>
+
+                        <button onClick={this.handleAddToCart} className="btn btn-primary" style={{ marginLeft: "50px" }}>Add to cart</button>
+
+
                     </div>
-                    <div className="col-md-4">
-             
-            </div>
-                    </div>
-              <div style={{marginTop: "50px"}}><button type="button" className="btn btn-primary">Share n Save</button>
-            
-            <button type="button" style={{marginLeft: "50px"}}>Buy Now</button>
-                     </div>  
                 </div>
             </div>
         );
