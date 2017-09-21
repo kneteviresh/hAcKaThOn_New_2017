8208512fce56f29444d2bf6d21eb8f8341af7e10
de36 <veeresh.kashappa@in.tesco.com>
2017-09-21 11:38:41 +0530

    latest

---
+++ dev/js/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Welcome to Share and Save kjkj</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
--- dev/js/index.js
+++ dev/js/index.js
@@ -4,12 +4,12 @@ import ReactDOM from "react-dom";
 import {Provider} from 'react-redux';
 import {createStore} from "redux";
 import allReducers from "./reducers/index";
-import TodoList from "./components/TodoList";
+import App from "./components/App";
 import { composeWithDevTools } from 'redux-devtools-extension'; 
 
 const store= createStore(allReducers,undefined,composeWithDevTools());
 ReactDOM.render(<Provider store={store}>
-<TodoList />
+<App />
 </Provider>,
        document.getElementById('root')
 );
