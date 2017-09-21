ddd30c7ab4dd748872ca8091e95ef2a1621162af
de36 <veeresh.kashappa@in.tesco.com>
2017-09-21 11:43:36 +0530

    basic setup

--- dev/js/components/App.js
+++ dev/js/components/App.js
@@ -4,7 +4,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <h1>Welcome to Share and Save kjkj</h1>
+                <h1>Welcome to Share and Save From <mark>LocalGhosts</mark></h1>
             </div>
         );
     }
