9984a287c42bdaefb2322e39dcd39765592facb6
Kavitha <xx18@global.tesco.org>
2017-09-21 13:15:32 +0530

    Added login screen fields

--- dev/js/components/login/login.js
+++ dev/js/components/login/login.js
@@ -6,7 +6,10 @@ export default class login extends React.Component {
     render(){
         return(
             <div className= "container">          
-                
+                <div className="panel panel-default">
+                    <div className="panel-heading">Login to Tesco app</div>
+                    <div className="panel-body">      
+                           
                         <div>
                          <input type="text" id="email" placeholder="Your Email Address" className = "login-user-input-email login-user-input" />
                         </div>
@@ -16,7 +19,10 @@ export default class login extends React.Component {
                         <div>
                             <button type="button" className="login-login-button"> Log in </button>
                         </div>
-                        </div>
+                    </div>
+                </div>
+            </div>
+            
                     
            
         )
